42e66533da65460cb4a8b523af26c7cfdc4b3b0c
mauridb <mbussi@alteanet.it>
2017-12-01 11:57:31 +0100

    LEARNING: understanding docker concepts amazing technology

---
+++ 07_DOCKER/app.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(bytes("Hello i m Mr Dockerfile", "utf8"))
+        return
+
+def run():
+    httpd = HTTPServer(('0.0.0.0', 8080), testHTTPServer_RequestHandler)
+    httpd.serve_forever()
+
+run()
